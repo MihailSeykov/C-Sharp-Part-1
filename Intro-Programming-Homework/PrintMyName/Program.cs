c326db510fb5cc050b368bfd30d91b01c7b4a403
Mihail Seykov <mihailseikov@gmail.com>
2015-01-09 21:32:07 +0200

    Intro To Programming Homework

---
+++ Intro-Programming-Homework/PrintMyName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintMyName
+{
+    class PrintMyName
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Intro-Programming-Homework/PrintNames/PrintNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class PrintNames
+{
+    static void Main()
+    {
+        Console.WriteLine("Mihail");
+        Console.WriteLine("Seykov");
+    }
+}
+
