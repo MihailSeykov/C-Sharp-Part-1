266f778d5160e301c6e66543e1439c4ac4269b42
Mihail Seykov <mihailseikov@gmail.com>
2015-01-14 16:38:15 +0200

    Homework

---
+++ Operators-And-Expresions-Homework/GravitationOnTheMoon/GravitationOnTheMoon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+    class GravitationOnTheMoon
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a weight: ");
+            double weight = double.Parse(Console.ReadLine());
+            double moon = 0.17; // This is the 17% * 0.01
+            double result = moon * weight;
+            Console.Write("This weight on the moon is: ");
+
+            
+            
+
+
+        }
+    }
+
---
+++ Operators-And-Expresions-Homework/OddOrEvenIntegers/OddOrEvenIntegers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class OddOrEvenIntegers
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number:");
+        int num = int.Parse(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("The number is even!");
+        }
+        else
+        {
+            Console.WriteLine("The number is odd!");
+        }
+    }
+}
+
