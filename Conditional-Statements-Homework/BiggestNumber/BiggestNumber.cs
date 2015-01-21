f934b793559fb53f81d371293fc5bdad63cf7840
Mihail Seykov <mihailseikov@gmail.com>
2015-01-21 18:00:23 +0200

    Homework

---
+++ Conditional-Statements-Homework/BiggestNumber/BiggestNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Write a program that finds the biggest of three numbers.
+
+class BiggestNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        double firstNumber = double.Parse(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        double secondNumber = double.Parse(Console.ReadLine());
+        Console.Write("Enter the third number: ");
+        double thirdNumber = double.Parse(Console.ReadLine());
+        Console.WriteLine("The biggest number is: " + Math.Max(Math.Max(firstNumber, secondNumber), thirdNumber));
+    }
+}
+
