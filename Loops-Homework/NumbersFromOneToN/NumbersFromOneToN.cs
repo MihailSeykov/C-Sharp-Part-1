67a65bd68ece49c49111faceaba3efaf11492db8
Mihail Seykov <mihailseikov@gmail.com>
2015-01-24 19:27:54 +0200

    Homework

---
+++ Loops-Homework/NumbersFromOneToN/NumbersFromOneToN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Write a program that enters from the console a positive integer n and prints all the numbers from 1 to n,
+//on a single line, separated by a space.
+
+class NumbersFromOneToN
+{
+    static void Main()
+    {
+        Console.Write("Enter a positive integer: ");
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= number; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
