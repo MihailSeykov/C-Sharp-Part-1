8140d273abeedcec9f090ed9f7cb5f2877a317d9
Mihail Seykov <mihailseikov@gmail.com>
2015-01-27 14:36:06 +0200

    Exam Preparation

---
+++ Exam-Preparation-6-December-2013-Morning/Exam-Preparation-6-December-2013-Morning/3-6-9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+    class Program
+    {
+        static void Main()
+        {
+            long a = long.Parse(Console.ReadLine());
+            long b = long.Parse(Console.ReadLine());
+            long c = long.Parse(Console.ReadLine());
+            long r = 0;
+            long result = 0;
+            if (b == 3)
+            {
+                r = a + c;
+            }
+            else if (b == 6)
+            {
+                r = a * c;
+            }
+            else if (b == 9)
+            {
+                r = a % c;
+            }
+            if (r % 3 == 0)
+            {
+                result = r / 3;
+                Console.WriteLine(result);
+                Console.WriteLine(r);
+            }
+            else if (r % 3 != 0)
+            {
+                result = r % 3;
+                Console.WriteLine(result);
+                Console.WriteLine(r);
+            }
+        }
+    }
+
---
+++ Exam-Preparation-6-December-2013-Morning/TheHorror/TheHorror.cs
@@ -0,0 +1,30 @@
+﻿using System;
+    class TheHorror
+    {
+        static void Main()
+        {
+            long number = long.Parse(Console.ReadLine());
+            long length = number.ToString().Length;
+            long digitSum = 0;
+            long digitsAmount = 0;
+            long digit = 0;
+            for (long position = 1; position <= length; position++)
+            {
+                if (number == 0)
+                {
+                    break;
+                }
+                digit = number % 10;
+                if (position % 2 == 0)
+                {
+                    digitSum = digitSum + digit;
+                    digitsAmount++;
+                }
+                number = number / 10;
+            }
+            Console.WriteLine("{0} {1}", digitsAmount, digitSum);
+            Main();
+        }
+       
+    }
+
